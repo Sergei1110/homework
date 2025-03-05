25bef2a78ea06bec1f95bd2670aa8374cd292a8f
Sergei1110 <146174015+Sergei1110@users.noreply.github.com>
2025-03-05 11:37:39 +0500

    Add files via upload

---
+++ lesson102d/main.js
@@ -0,0 +1,46 @@
+const myPage = {
+  name: 'Sergei',
+  age: 40,
+  height: 185,
+  isAdmin: false,
+  greet: function (name) {
+    return `Hello ${name}`;
+  }
+};
+
+console.log(myPage.greet('Sergei'))
+
+const users = [
+  {
+    name: 'Alex',
+    age: 25,
+    isAdmin: false
+  }, {
+    name: 'John',
+    age: 35,
+    isAdmin: false
+  }, {
+    name: 'Oleg',
+    age: 38,
+    isAdmin: false
+  }, {
+    name: 'Ivan',
+    age: 28,
+    isAdmin: true
+  }
+]
+
+let usersCount = 0
+
+for (let i = 0; i < users.length; i++) {
+  if (!users[i].isAdmin) {
+    usersCount++;
+  }
+}
+
+console.log(usersCount)
+
+
+
+
+
